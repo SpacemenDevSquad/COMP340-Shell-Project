1711826c6cc5e457d0dd313be019735bb07eb0fe
Peter Brumbach <contact@peterbrumbach.com>
2026-02-19 19:36:31 -0500

    Wow

---
+++ problem2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
+int shell_file_exists(char *file_path) {
+  // use the stat() system call to check if a file exists
+}
+
+int main (int argc, char *argv[]) {
+
+}
